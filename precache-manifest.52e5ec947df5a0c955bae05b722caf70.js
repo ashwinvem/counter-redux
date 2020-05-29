8f3f392ad2afeca25ec5125a96d559bde9982cc0
Sunil Kashyap <sunil@innocirc.com>
2020-05-30 01:20:45 +0530

    Updates

---
+++ precache-manifest.52e5ec947df5a0c955bae05b722caf70.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f44296e81290b11a7973e300e9ec60bc",
+    "url": "/counter-redux/index.html"
+  },
+  {
+    "revision": "b283dd6ea8d52e9dd80d",
+    "url": "/counter-redux/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "35c6ad2c446cb312d322",
+    "url": "/counter-redux/static/css/main.e26ea096.chunk.css"
+  },
+  {
+    "revision": "b283dd6ea8d52e9dd80d",
+    "url": "/counter-redux/static/js/2.405fab71.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/counter-redux/static/js/2.405fab71.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "35c6ad2c446cb312d322",
+    "url": "/counter-redux/static/js/main.66fcb925.chunk.js"
+  },
+  {
+    "revision": "5204c29de3c1ad199849",
+    "url": "/counter-redux/static/js/runtime-main.30ce9957.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/counter-redux/static/media/logo.5d5d9eef.svg"
+  }
+]);
